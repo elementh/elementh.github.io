fc5b69d9f8a0595eede8a427a62b925efa27eea5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2017-01-03 15:44:02 +0100

    New things

--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-console.log('There is nothing to see here lulz - Lucas\n\n')
-console.log('As you are here, why don\'t you give Archlinux a try? A simple, lightweight distribution Linux  distribution that tries to Keep It Simple!')
-console.log('https://www.archlinux.org/')
